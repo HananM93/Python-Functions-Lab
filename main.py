4818cd996636d9fe5392c4c0d776267bac1b0c97
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-21 01:30:24 -0600

    done question #1

---
+++ main.py
@@ -0,0 +1,10 @@
+# Challenges 
+# 1. Write a function named sum_tothat accepts a single integer, n, and returns the sum of the integers from 1 to n.
+# For example:
+# sum_to(6)  # returns 21
+# sum_to(10) # returns 55
+
+def sum_to(n):
+    return print(n * (n + 1) // 2)
+
+sum_to(6)
